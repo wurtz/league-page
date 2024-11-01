dc4bfc144fa77f73cf9c038f12342eebfda51aeb
wurtz <djdubw@gmail.com>
2024-11-01 17:30:05 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -188,7 +188,7 @@ export const managers = [
       "managerID": "1093032218567581696",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
       "name": "Ryne",
       "tookOver": 2020, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
-      "location": "Oklahoma or some shit", // (optional)
+      "location": "Fort Leonard Wood, Missouri", // (optional)
       "bio": "Lorem ipsum...",
       "photo": "/managers/ryne.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
