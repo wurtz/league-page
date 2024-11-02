d8b487496a8bdb867659b6020e1a4a718ca547c3
wurtz <djdubw@gmail.com>
2024-11-02 01:34:28 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -8,6 +8,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 /*   STEP 2   */
 export const homepageText = `
   <p>Dirty Dozen was founded in 2012 in the bowels of America AKA Florida. Over the years we've had folks come and go but our core group hasn't changed. In 2024 with much controversy we left Yahoo after a decade, and transitioned to Best Ball.</p>
+  <p>Sleeper's rich API has enabled us to build this sweet ass site.</p>
   <p>While we're all here for the glory, we're also playing for that sweet sweet cash.</p>
   <ul>
       <li>1st Place: $400</li>
