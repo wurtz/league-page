f36644fa5e6aa566a4046993dcc0610a3e5ed9a8
wurtz <djdubw@gmail.com>
2024-11-03 08:27:21 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -17,15 +17,14 @@ export const homepageText = `
   </ul> 
    <p>Here's our Championship Hall of Fame exported from Yahoo going back to 2017 (I lost everything before that).</p>
      <ul>
-      <li>🏆2017:<a href="https://www.dirtydozen.win/manager/?manager=0"> Brad</a>🏆</li>
-      <li>🏆2018:<a href="https://www.dirtydozen.win/manager/?manager=3"> Johnny</a>🏆</li>
-      <li>🏆2019:<a href="https://www.dirtydozen.win/manager/?manager=4"> Austin</a>🏆</li>
-      <li>🏆2020:<a href="https://www.dirtydozen.win/manager/?manager=7"> Ryne</a>🏆</li>
-      <li>🏆2021:<a href="https://www.dirtydozen.win/manager/?manager=2"> Corey</a>🏆</li>
+      <li>🏆2023:<a href="https://www.dirtydozen.win/manager/?manager=10"> Devin</a>🏆</li> 
       <li>🏆2022:<a href="https://www.dirtydozen.win/manager/?manager=1"> David</a>🏆</li>
-      <li>🏆2023:<a href="https://www.dirtydozen.win/manager/?manager=10"> Devin</a>🏆</li>
-      
-  </ul> 
+      <li>🏆2021:<a href="https://www.dirtydozen.win/manager/?manager=2"> Corey</a>🏆</li>
+      <li>🏆2020:<a href="https://www.dirtydozen.win/manager/?manager=7"> Ryne</a>🏆</li>
+      <li>🏆2019:<a href="https://www.dirtydozen.win/manager/?manager=4"> Austin</a>🏆</li>
+      <li>🏆2018:<a href="https://www.dirtydozen.win/manager/?manager=3"> Johnny</a>🏆</li>
+      <li>🏆2017:<a href="https://www.dirtydozen.win/manager/?manager=0"> Brad</a>🏆</li> 
+    </ul> 
   <br>
   <br>
   <img src="trash.png" alt="traaaaaaaaaaash">
