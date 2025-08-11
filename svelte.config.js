a69929e9f87acd6349f3afcb28084365c2f27af0
wurtz <djdubw@gmail.com>
2025-08-10 21:01:12 -0400

    fix: specify vercel runtime for Node.js 22 compatibility

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
 import vercel from '@sveltejs/adapter-vercel';
 import node from '@sveltejs/adapter-node';
 
-const dockerBuild = process.env.DOCKER_BUILD
+const dockerBuild = process.env.DOCKER_BUILD;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: dockerBuild ? node() : vercel(),
-	}
+  kit: {
+    adapter: dockerBuild
+      ? node()
+      : vercel({
+          runtime: 'nodejs20.x'
+        }),
+  }
 };
 
 export default config;
