491215e6dec076073dbf94fdb5349d73f886bd61
wurtz <djdubw@gmail.com>
2025-08-10 21:22:48 -0400

    final fix: use nodejs18.x with adapter-vercel@2.2.1

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
     adapter: dockerBuild
       ? node()
       : vercel({
-          runtime: 'nodejs20.x'  // ✅ Vercel now requires at least Node 20 for functions
+          runtime: 'nodejs18.x'
         }),
   }
 };
