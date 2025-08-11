75661997c70b1e1b41014bee97370ea096096793
nissim <djdubw@gmail.com>
2025-08-11 10:24:31 -0400

    Update leagueInfo.js & add clowns

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
 export const leagueID = "1092949297970176000"; // your league ID
-export const leagueName = "🤑 Dirty Dozen 🤑"; // your league name
+export const leagueName = "🤡 Dirty Dozen 🤡"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
