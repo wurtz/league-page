800c1696a9de24ac47e858346bade2a60dc931cb
wurtz <djdubw@gmail.com>
2025-09-02 23:52:01 -0400

    updated leagueID

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1092949297970176000"; // your league ID
+export const leagueID = "1257481843356225536"; // your league ID
 export const leagueName = "🤡 Dirty Dozen 🤡"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
